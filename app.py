45c6a5157184bea28a36c80934f5c1537f661d06
Ren-Korat-24 <koratren12@gmail.com>
2025-10-28 10:11:57 +0530

    loop in class

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,request,redirect,url_for,session,Response,render_template
+
+# Create a Flask app instance
+app = Flask(__name__)
+app.secret_key='supersecret'
+
+#Home Page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+#Form page
+@app.route('/about')
+def about():
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
