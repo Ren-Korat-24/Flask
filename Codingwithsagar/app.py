5cacc067a4ec297f4547f3d6dacf5db48c39b58d
Ren-Korat-24 <koratren12@gmail.com>
2025-10-31 09:29:12 +0530

    flash

--- Codingwithsagar/app.py
+++ Codingwithsagar/app.py
@@ -1,18 +1,25 @@
-from flask import Flask,request,redirect,url_for,session,Response,render_template
+from flask import Flask,request,redirect,flash,url_for,session,Response,render_template
 
 # Create a Flask app instance
 app = Flask(__name__)
 app.secret_key='supersecret'
 
 #Home Page
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form.get('name')
+        if not name:
+            flash('Name is required!')
+            return redirect(url_for("form"))  
+        flash(f'Thanks {name},your feedback was saved!')
+        return redirect(url_for("thankyou"))  
+    return render_template('form.html')
 
 #Form page
-@app.route('/about')
-def about():
-    return render_template('form.html')
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
